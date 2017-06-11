21a88356ba36311327d0f818f779350ac07962a5
jfseb <jfsebpublic@gmail.com>
2017-06-11 16:55:46 +0200

    nommalized pathes, dummy file

--- js/constants.js
+++ js/constants.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const path = require('path');
 exports.MONGO_DBURL = 'mongodb://localhost/testdb';
-exports.MONGOOSE_RECORD_REPLAY_FOLDER = __dirname + '/../mgrecrep/';
-exports.RAW_MODEL_PATH = __dirname + '/../testmodel';
-exports.MODEL_PATH = __dirname + '/../testmodel';
+exports.MONGOOSE_RECORD_REPLAY_FOLDER = path.normalize(__dirname + '/../mgrecrep/');
+exports.RAW_MODEL_PATH = path.normalize(__dirname + '/../testmodel');
+exports.MODEL_PATH = path.normalize(__dirname + '/../testmodel');
 exports.ENV_NAME_MONGO_RECORD_REPLAY = 'MGNLQ_TESTMODEL_REPLAY';
 
 //# sourceMappingURL=constants.js.map
--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 
+const path = require('path');
 
 export const MONGO_DBURL =  'mongodb://localhost/testdb';
-export const MONGOOSE_RECORD_REPLAY_FOLDER = __dirname + '/../mgrecrep/';
-export const RAW_MODEL_PATH = __dirname + '/../testmodel';
-export const MODEL_PATH = __dirname + '/../testmodel';
+export const MONGOOSE_RECORD_REPLAY_FOLDER = path.normalize(__dirname + '/../mgrecrep/');
+export const RAW_MODEL_PATH = path.normalize(__dirname + '/../testmodel');
+export const MODEL_PATH = path.normalize(__dirname + '/../testmodel');
 export const ENV_NAME_MONGO_RECORD_REPLAY = 'MGNLQ_TESTMODEL_REPLAY';
